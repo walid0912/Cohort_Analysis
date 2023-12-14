bfe16126ed267d8b2379cd19416def8d3ae11975
Walid KROUCHI <117538373+walid0912@users.noreply.github.com>
2023-12-14 10:25:13 +0100

    Update cohort.py

--- cohort.py
+++ cohort.py
@@ -88,3 +88,14 @@ fig2.update_yaxes(title='Average Duration')
 
 fig1.show()
 fig2.show()
+
+# Creating a cohort matrix
+cohort_matrix = weekly_averages.set_index('Week')
+
+# Plotting the cohort matrix
+plt.figure(figsize=(12, 8))
+
+sns.heatmap(cohort_matrix, annot=True, cmap='coolwarm', fmt=".1f")
+plt.title('Cohort Matrix of Weekly Averages')
+plt.ylabel('Week of the Year')
+plt.show()
