c3fdab261c0aa34382beaebe4afd7cb5b18e9b66
Walid KROUCHI <117538373+walid0912@users.noreply.github.com>
2023-12-14 10:20:51 +0100

    Data types of the different columns

--- cohort.py
+++ cohort.py
@@ -7,3 +7,8 @@ print(data.head())
 # Checking if there's any missing values or null values 
 missing_values = data.isnull().sum()
 print(missing_values)
+
+# Data types of the different columns
+data_types = data.dtypes
+print(data_types)
+
